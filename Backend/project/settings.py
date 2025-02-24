adc77f658ca484f3d559f918c470ee86cc07494a
boualif <farah.bouali.orelservices@gmail.com>
2025-02-24 11:50:32 +0100

    Added gunicorn to requirements.txt

--- Backend/project/settings.py
+++ Backend/project/settings.py
@@ -336,6 +336,8 @@ LOCALE_PATHS = [
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
